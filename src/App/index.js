1027b8396a58520a6ea643e5a6b446ca2a8b3386
blarth <jminocente@gmail.com>
2022-01-03 00:52:06 -0300

    Instalando bibliotecas externas e setando o path inicial de paginas

---
+++ src/App/index.js
@@ -0,0 +1,18 @@
+
+import MainPage from "../MainPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      
+      <Routes>
+        <Route path="/" exact element={<MainPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
